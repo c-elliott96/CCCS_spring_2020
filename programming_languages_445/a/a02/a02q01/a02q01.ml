7f6c3d27511bf2c578f2dbfa5a329e66e0212942
Christian Elliott <cdelliott96@gmail.com>
2020-01-22 14:28:38 -0800

    fixed a02a01.ml

--- programming_languages_445/a/a02/a02q01/a02q01.ml
+++ programming_languages_445/a/a02/a02q01/a02q01.ml
@@ -6,6 +6,9 @@
 exception IgnoreCare;;
 exception NotImplemented;;
 
-let max = x (fun -> (fun -> y -> (fun z -> if y > z then y else z)));;
+let max = fun max -> (fun x -> (fun y -> (if x > y then x else y)));;
 
 let f = max 1 2 3;;
+print_int f;;
+print_newline;;
+
