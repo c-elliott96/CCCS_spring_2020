f791d3f51e584f428c8bf785bf5702472223ac46
Christian Elliott <cdelliott96@gmail.com>
2020-01-22 22:56:57 -0800

    nothing significant done

--- programming_languages_445/a/a02/a02q01/a02q01.ml
+++ programming_languages_445/a/a02/a02q01/a02q01.ml
@@ -6,9 +6,17 @@
 exception IgnoreCare;;
 exception NotImplemented;;
 
-let max = fun max -> (fun x -> (fun y -> (if x > y then x else y)));;
+(* let max = fun max -> (fun x -> (fun y -> (if x > y then x else y))) ;;
+   let max = fun max -> fun x -> fun y -> if x > y then x else y;;
+   *)
+   
+let max = fun x ->
+fun y ->
+fun z ->
+if y > z then y
+else
+z;;
 
 let f = max 1 2 3;;
 print_int f;;
-print_newline;;
-
+print_string " \n";;
