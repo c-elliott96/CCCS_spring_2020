36cb59d642c0a442415529af5996fd0be329ac20
Christian Elliott <cdelliott96@gmail.com>
2020-01-23 20:58:38 -0800

    finished a02q02! on a Roll!

---
+++ programming_languages_445/a/a02/a02q03/a02q03.ml
@@ -0,0 +1,41 @@
+(*
+  File: a02q02.ml
+  Name: Christian Elliott
+ *)
+
+exception IgnoreCase;;
+exception NotImplemented;;
+
+(* end boilerplate *)
+
+(* let f x = x +. 1.0;;
+ * let g x = x -. 1.0;;
+ * let h x = x *. x;; *)
+
+let add_func f g = fun x -> (f x) +. (g x);;
+
+(* let one = (add_func f g) 0.0;;
+ * let two = (add_func f g) 1.0;;
+ * let three = (add_func g f) 2.0;;
+ * let four = (add_func g f) 3.0;;
+ * let five = (add_func f h) 0.0;;
+ * let six = (add_func f h) 1.0;;
+ * let seven = (add_func h f) 2.0;;
+ * 
+ * 
+ * print_float one;;
+ * print_string " ";
+ * print_float two;;
+ * print_string " ";
+ * print_float three;;
+ * print_string " \n";;
+ * print_float four;;
+ * print_string " \n";;
+ * print_float five;;
+ * print_string " \n";;
+ * print_float six;;
+ * print_string " \n";;
+ * print_float seven;;
+ * print_string " \n";; *)
+
+(* let add_func f g = fun x -> f +. (g x);; *)
