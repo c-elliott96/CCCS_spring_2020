2861b04a8cb79dc58dfe1b60c513691c0fba3d97
Christian Elliott <cdelliott96@gmail.com>
2020-02-13 15:51:28 -0600

    working on a02 db

--- databases_430/a/a02/a02q01/main.sql
+++ databases_430/a/a02/a02q01/main.sql
@@ -3,49 +3,51 @@ create database cms;
 use cms;
 
 create table year (
-       id INT,
-       year YEAR(4),
+       id INT AUTO_INCREMENT,
+       year INT,
        primary key (id)
-       );
+       ) engine=innodb;
 
 create table season (
-       id INT,
+       id INT AUTO_INCREMENT,
        season VARCHAR(100),
        primary key (id)
-       );
+       ) engine=innodb;
 
 create table semester (
-       id INT,
+       id INT AUTO_INCREMENT,
        sem_start DATE,
-       sem_end DATE
+       sem_end DATE,
        primary key (id)
-       );
+       ) engine=innodb;
 
 create table course (
-       id INT,
+       id INT AUTO_INCREMENT,
        course VARCHAR(100),
        primary key (id)
-       );
+       ) engine=innodb;
 
 create table section (
-       id INT,
+       id INT AUTO_INCREMENT,
        course VARCHAR(10),
        primary key (id)
-       );
+       ) engine=innodb;
 
 create table person (
-       id INT,
+       id INT AUTO_INCREMENT,
        fname VARCHAR(100),
        lname VARCHAR(100),
        email VARCHAR(100),
        user VARCHAR(100),
        password VARCHAR(100),
        primary key (id)
-       );
+       ) engine=innodb;
 
 create table instructor (
+       id INT NOT NULL,
+       primary key(id),
        person_id INT,
        foreign key (person_id) references person(id)
-       );
+       ) engine=innodb;
 
 
