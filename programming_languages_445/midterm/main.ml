982b6222fa8d1b709575b1a3f6dd84f6b53d7a78
Christian Elliott <cdelliott96@gmail.com>
2020-03-23 12:25:00 -0700

    worked on ocaml midterm

--- programming_languages_445/midterm/main.ml
+++ programming_languages_445/midterm/main.ml
@@ -30,12 +30,15 @@ let main () =
     let _ = print_string
           "Welcome to Tic-Tac-Toe Ocaml! Enter the size of the board: " in
     let n = read_int () in
-    play n (initBoard n) 'X'
+    let board = initBoard n in
+    get_player_move board n
 ;;
            
      
 main ();; 
 
+
+(* the logic in main needs to be adjusted. I need to be able to reference board by name *)
 
 (* this function works - it prompts for a number until it receives 42
 let rec play () = 
@@ -47,36 +50,66 @@ else
     print_string "Incorrect. Try again.\n" in 
     play ()
 ;;
-
-(* this function checks to see if a play is valid *)
-let player_move_validation move board = 
-  if board.(move) == ' ' then
-    board.(move) <- 'X'
-  else print_string "Invalid move; please try again.\n"
  *)
 
+(* this function checks to see if a play is valid *)
+(* let player_move_validation move board = 
+ *   if board.(move) == ' ' then
+ *     board.(move) <- 'X'
+ *   else print_string "Invalid move; please try again.\n"
+ * ;; *)
+
+(* bool version of function immediately above *)
+let player_move_validation move board =
+  board.(move) == ' ';;
+
+
+let make_player_move move board =
+  board.(move) <- 'X'
+;;
+
 (* make a board index calculation function *)
-(* let calculate_move row col n =
- *   (n * row) + col;; *)
+let calculate_move row col n =
+  (n * row) + col;;
 
 
 (* here is how we will take in the player's move,
    check to ensure that the spot is unoccupied, 
    and then modify the board array if true, 
    if false, ask for a new move *)
-(* let rec get_player_move board n =
- *   let _ = print_string "row: " in
- *   let row = read_int () in
- *   let _ = print_string "col: " in
- *   let col = read_int () in
- *   let move = calculate_move row col n in 
- *   if (player_move_validation move board) then
- *     (\* if true, it's a valid move. *\)
- *     board.(move) <- 'X'
- *   else
- *     let _ = print_string "Invalid move. Please try again.\n" in
- *     get_player_move board n
+let rec get_player_move board n =
+  let _ = print_string "row: " in
+  let row = read_int () in
+  let _ = print_string "col: " in
+  let col = read_int () in
+  let move = calculate_move row col n in 
+  if (player_move_validation move board) then
+    (* if true, it's a valid move. *)
+    board.(move) <- 'X'
+  else
+    let _ = print_string "Invalid move. Please try again.\n" in
+    get_player_move board n
+;;
+
+(* let player_row_check board row col n = 
+ *   let ref col_ref = 0 in
+ *   let winning_move = 
+ *   for row_ref = 0 to n do
+ *     let move = calculate_move !row_ref !col_ref n in
+ *     let winning_move = board.(move) = 'X';
+ *   done
  * ;; *)
 
-let rec player_row_check
-          (* finish implementing this function! *) 
+(* this function checks row of board to see if all = 'X' *) 
+let check_row board row n = 
+let temp = Array.sub board (row * n) n in
+Array.for_all ((=) 'X') temp
+;;
+
+
+let check_all_rows board n =
+  let winner =
+    for i to n do
+      
+                  
+   
